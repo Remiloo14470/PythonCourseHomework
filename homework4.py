7f22e6f91d3094882a81019a00d060612515bab6
Remiloo14470 <raminapolkina@gmail.com>
2024-07-10 15:27:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите любой текст ')
+print('Количество символов: ', len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print('Первый символ:', my_string[0])
+print("Последний символ:", my_string[-1])
