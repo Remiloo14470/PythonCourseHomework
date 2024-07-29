951c8d93bbfe0c63faa12425e6e94102f2f64cd6
Remiloo14470 <raminapolkina@gmail.com>
2024-07-29 18:39:32 +0300

    Add files via upload

--- Module5_1.py
+++ Module5_1.py
@@ -9,11 +9,14 @@ class House:
 
     def go_to(self, new_floor):
         self.new_floor = new_floor
-        new_floor = random.randint(1, new_floor)
-        print(new_floor)
+        #new_floor = random.randint(1, new_floor)
+        for k in range(1, new_floor+1):
+            print(k)
         if new_floor > self.number_of_floors or new_floor <1:
             print('Такого этажа не существует')
 
 
-dom = House('ЖК Сердце города', 18)
-dom.go_to(90)
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
