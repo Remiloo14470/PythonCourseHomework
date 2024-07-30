c87f34592e9e089fd86180d1dd82d608c0fd60a2
Remiloo14470 <raminapolkina@gmail.com>
2024-07-30 16:57:22 +0300

    Add files via upload

---
+++ fakeMath.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    if first > 0 and second > 0:
+        div = first / second
+        return div
+    if second == 0:
+        string = 'Ошибка'
+        return string
+
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fakeMath import divide as dif
+from trueMath import divide as dit
+
+result1 = dif(69, 3)
+result2 = dif(3, 0)
+result3 = dit(49, 7)
+result4 = dit(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
