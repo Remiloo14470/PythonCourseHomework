cc9cf734cd77eac715c713d6f52da6738278cb9a
Remiloo14470 <raminapolkina@gmail.com>
2024-10-08 14:39:33 +0300

    Module11_2.py

--- Module11_2.py
+++ Module11_2.py
@@ -3,12 +3,25 @@ from pprint import pprint
 
 
 def introspection_info(obj):
-    methods = [m[0] for m in inspect.getmembers(obj, predicate=inspect.isfunction(obj))]
-    keys_dict = ['type', 'attributes', 'methods', 'module']
-    values_dict = [type(obj), dir(obj), methods, inspect.getmodule(introspection_info)]
-    obj_dict = dict(zip(keys_dict, values_dict))
-    return obj_dict
+
+    info = {}
+
+    info['type'] = type(obj).__name__
+
+    all_attributes = dir(obj)
+
+    attributes = [attr for attr in all_attributes if not callable(getattr(obj, attr))]
+    methods = [method for method in all_attributes if callable(getattr(obj, method))]
+
+    info['attributes'] = attributes
+    info['methods'] = methods
+
+    info['module'] = obj.__module__ if hasattr(obj, '__module__') else 'No module found'
+
+    info['doc'] = obj.__doc__ if hasattr(obj, '__doc__') else 'No documentation available'
+
+    return info
 
 
 number_info = introspection_info(42)
-pprint(number_info)
+print(number_info)
