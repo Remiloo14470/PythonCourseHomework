2838f67c87a6b1b49404c8f39056d02802a55ace
Remiloo14470 <raminapolkina@gmail.com>
2024-10-23 13:26:57 +0300

    Module13_5.py

--- Module13_5.py
+++ Module13_5.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = "7860471206:AAG6mzapA0JqhNizrXHPFHPKjmZuV8aKyIg"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
