f4e4c8f27c0555c4d55eb96cd61dc92e1ab793f1
Remiloo14470 <raminapolkina@gmail.com>
2024-11-21 15:57:05 +0300

    Module16_4.py

--- Module16_4.py
+++ Module16_4.py
@@ -20,29 +20,30 @@ async def get_users() -> List[User]:
 @app.post("/user/{username}/{age}")
 async def add_user_func(username: Annotated[str, Path(max_length=15, min_length=3, description='Enter you username', example='Remiloo')],
                         age: int = Path(ge=0, le=100, description='Enter your age', example='23')) -> User:
-    user_id = len(users)
+    user_id = len(users) + 1
     user = User(id=user_id, username=username, age=age)
     users.append(user)
     return user
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user_info(user_id: int, username: str, age: int) -> List[User]:
+async def update_user_info(user_id: int, username: str, age: int) -> User:
     try:
         for user in users:
             if user_id == user.id:
-                user.username = username
-                user.age = age
-                return users
+                user_updated = User(id=user_id, username=username,age=age)
+                return user_updated
     except IndexError:
         raise HTTPException(status_code=404, detail='User not found')
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: int) -> List[User]:
+async def delete_user(user_id: int) -> User:
     try:
-        users.pop(user_id)
-        return users
+        for user in users:
+            if user_id == user.id:
+                users.remove(user)
+                return user
     except IndexError:
         raise HTTPException(status_code=404, detail='User not found')
 
