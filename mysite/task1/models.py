f083ea6966ef2590fac91607b428033002830007
raminapolkina <raminapolkina@gmail.com>
2024-12-09 14:34:35 +0300

    Project_19

---
+++ mysite/task1/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=30, name='username')
+    balance = models.DecimalField(max_digits=5, decimal_places=3)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=5, decimal_places=3)
+    size = models.DecimalField(max_digits=5, decimal_places=3)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
