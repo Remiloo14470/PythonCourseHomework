0d832b886699f3a53775e370334c93773c12a6d7
Ramina Zarakhovich <raminapolkina@gmail.com>
2025-03-10 10:12:26 +0300

    Rename homework2.py to Module_1/homework2.py

--- homework2.py
+++
@@ -1,5 +0,0 @@
-Homework_number = 12
-Hours_number = 1.5
-Course_name = 'Python'
-Time_per_homework = Hours_number / Homework_number
-print('Курс:',Course_name,',всего задач:',Homework_number,',затрачено часов:',Hours_number,',среднее время выполнения:',Time_per_homework,' часа')
